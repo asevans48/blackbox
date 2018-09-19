9a90034e6ac8121ddb61a0d17aecd268d835f8eb
aevans <aevans48@simplrsinsites.com>
2018-09-18 20:51:45 -0600

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Blackbox Python API',
+    version='0.1dev',
+    packages=['blackbox',],
+    license='Apachev2.0',
+    long_description=open('README.txt').read(),
+)
